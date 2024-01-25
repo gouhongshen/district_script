db3ac26c3cb74d64ff0112bda6be2f42e466c676
gouhongshen <gouhongshen@hotmail.com>
2024-01-25 16:18:57 +0800

    wide_mutli_indexes

--- insert_benchmark/standalone_test.go
+++ insert_benchmark/standalone_test.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"os/exec"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
 	"testing"
@@ -71,6 +72,16 @@ func createNoPKTable() *gorm.DB {
 	if *keepTbl <= 0 {
 		db.Exec("drop table no_pk_tables")
 		db.Exec("create table no_pk_tables (a bigint, b bigint, c bigint, d varchar, e bigint)")
+	}
+
+	return db
+}
+
+func createWideMIndexesTable(createSql string) *gorm.DB {
+	db := connect2DB("wide_indexes_db")
+	if *keepTbl <= 0 {
+		db.Exec("drop table wide_indexes_table")
+		db.Exec(createSql)
 	}
 
 	return db
@@ -288,7 +299,7 @@ func InsertWorker(
 }
 
 func cpuMemWorker(ctx context.Context, ch chan struct{}) {
-	//time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 10)
 	var cpuUsage []float64
 	var memUsage []float64
 
@@ -440,6 +451,87 @@ func generateStatementCUValues(s, e int, offset int64) (string, time.Duration) {
 
 		values = append(values,
 			fmt.Sprintf("('%s','%s','%s',%d,%d)", stmtId, account, responseAt, cu, accountId))
+	}
+	return strings.Join(values, ","), time.Since(start)
+}
+
+func Test_WideMIndexedTable_Insert(t *testing.T) {
+	createTableSql := "create table wide_indexes_table (" +
+		"`id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '自增主键'," +
+		"`company_id` bigint(0) NOT NULL COMMENT '所属企业Id，作为分区字段，与自增长id作为联合主键'," +
+		"`code` varchar(32)  NULL DEFAULT NULL COMMENT '车企编码 kafak导入'," +
+		"`vccodedosedf` varchar(32)  NULL DEFAULT NULL COMMENT 'vccodedosedf'," +
+		"`platform_id` varchar(64)  NULL DEFAULT NULL COMMENT '平台ID'," +
+		"`subs_id` varchar(32)  NULL DEFAULT NULL COMMENT '用户id kafak导入'," +
+		"`uuid` varchar(64)  NOT NULL DEFAULT '' COMMENT '唯一标识'," +
+		"`create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'," +
+		"`create_by` varchar(255)  NULL DEFAULT NULL COMMENT '创建者'," +
+		"`update_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间'," +
+		"`update_by` varchar(255)  NULL DEFAULT NULL COMMENT '更新者'," +
+		"`icciddfaf` varchar(32)  NOT NULL COMMENT 'ICCID'," +
+		"`imsiopld` varchar(32)  NULL DEFAULT NULL COMMENT 'imsiopld kafak导入'," +
+		"`msisdn123` varchar(32)  NULL DEFAULT NULL COMMENT 'kafak导入'," +
+		"`carrier` int(0) NOT NULL COMMENT '1:中国移动，1:中国电信，3:中国联通'," +
+		"`imei` varchar(32)  NULL DEFAULT NULL COMMENT 'IMEI'," +
+		"`vinodk` varchar(32)  NULL DEFAULT NULL COMMENT '车辆VIN码'," +
+		"`open_` datetime(3) NULL DEFAULT NULL COMMENT '日期'," +
+		"`active` datetime(3) NULL DEFAULT NULL COMMENT '日期'," +
+		"`network_type` varchar(8)  NULL DEFAULT NULL COMMENT '网络类型（字典项编码）'," +
+		"`card_type` varchar(8)  NULL DEFAULT NULL COMMENT '卡片物理类型（字典项编码）'," +
+		"`belong_place` varchar(32)  NULL DEFAULT NULL COMMENT '归属地'," +
+		"`remark` varchar(255)  NULL DEFAULT NULL COMMENT '备注'," +
+		"`status_time` datetime(0) NULL DEFAULT NULL COMMENT '卡号状态变更时间'," +
+		"`vehicle_status` int(0) NULL DEFAULT NULL COMMENT '车辆状态'," +
+		"`vehicle_out_factory_time` datetime(3) NULL DEFAULT NULL COMMENT '车辆出厂时间'," +
+		"`status` int(0) NOT NULL DEFAULT 0 COMMENT ''," +
+		"`stattu1` int(0) NULL DEFAULT NULL COMMENT ''," +
+		"`account_id` varchar(32)  NULL DEFAULT NULL COMMENT '账户id'," +
+		"`account_name` varchar(64)  NULL DEFAULT NULL COMMENT '账户名称'," +
+		"`plat_type` varchar(8)  NULL DEFAULT NULL COMMENT '平台类型2:pb 3:ct'," +
+		"`cust_id` varchar(32)  NULL DEFAULT NULL COMMENT '客户id kafak导入'," +
+		"`cust_name` varchar(32)  NULL DEFAULT NULL COMMENT '客户名称'," +
+		"`cust_type` varchar(32)  NULL DEFAULT NULL COMMENT '客户类型 一般为C客户类型'," +
+		"`be_id` varchar(32)  NULL DEFAULT NULL COMMENT '省份编码'," +
+		"`region_id` varchar(32)  NULL DEFAULT NULL COMMENT '归属地编码'," +
+		"`group_id` varchar(128)  NULL DEFAULT NULL COMMENT '归属群组id'," +
+		"`group_member_status` varchar(128)  NULL DEFAULT NULL COMMENT '归属群组中成员状态'," +
+		"`data_usage` bigint(0) NULL DEFAULT NULL COMMENT '用量'," +
+		"`cust` varchar(255)  NULL DEFAULT NULL COMMENT '编码'," +
+		"`sync_time` datetime(0) NULL DEFAULT NULL COMMENT '通过kafka入库时，每次必须更新的字段，其他入口不用变动'," +
+		"`source_create_time` datetime(0) NULL DEFAULT NULL COMMENT '源端数据创建时间'," +
+		"`source_modify_time` datetime(0) NULL DEFAULT NULL COMMENT '源端数据修改时间'," +
+		"`boss` int(0) NOT NULL COMMENT '运营商BOSS系统，1-移动CT，2-移动PB，3-电信DCP，4-电信M2M，5-联通jasper，6-联通CMP，7-中国电信5GCMP平台'," +
+		"`card_status` varchar(20)  NULL DEFAULT NULL COMMENT '卡状态'," +
+		"`device_num` varchar(64)  NULL DEFAULT NULL COMMENT '设备号'," +
+		"PRIMARY KEY (`id`) ," +
+		"UNIQUE INDEX `uk_ecbase_card_vc_icciddfaf`(`vccodedosedf`, `icciddfaf`)  COMMENT '20231021新增'," +
+		"INDEX `ecbase_card_ecbase_company_id_fk`(`company_id`) ," +
+		"INDEX `ecbase_card_subs_id`(`subs_id`) ," +
+		"INDEX `idx_ecbase_card_vin`(`vinodk`) ," +
+		"INDEX `ecbase_card_code_carrier_card_status_idx`(`code`, `carrier`) ," +
+		"INDEX `ecbase_card_code_vehicle_status_idx`(`code`, `vehicle_status`) ," +
+		"INDEX `idx_account_id`(`account_id`) ," +
+		"INDEX `idx_ecbase_card_msisdn123`(`msisdn123`) ," +
+		"INDEX `ecbase_card_code_icciddfaf_idx`(`code`, `icciddfaf`) ," +
+		"INDEX `idx_vc`(`vccodedosedf`) ," +
+		"INDEX `ecbase_card_icciddfaf`(`icciddfaf`)  COMMENT '2310.7新增'" +
+		");"
+
+	db := createWideMIndexesTable(createTableSql)
+	prefix := "(company_id, code, vccodedosedf, platform_id, subs_id, uuid, create_time, create_by, update_time, update_by, icciddfaf, imsiopld, msisdn123, carrier, imei, vinodk, open_, active, network_type, card_type, belong_place, remark, status_time, vehicle_status, vehicle_out_factory_time, status, stattu1, account_name, plat_type, cust_id, cust_name, cust_type, be_id, region_id, group_id, group_member_status, data_usage, cust, sync_time, source_create_time, source_modify_time, boss, account_id, card_status, device_num) "
+	tableName := "wide_indexes_table" + prefix
+	InsertWorker(db, tableName, generateWideMIndexesValues)
+}
+
+func generateWideMIndexesValues(s, e int, offset int64) (string, time.Duration) {
+	start := time.Now()
+	var values []string
+	for idx := s; idx < e; idx++ {
+		accompanyId := int64(idx) + offset
+
+		values = append(values,
+			fmt.Sprintf("(%d, '334', '$unique', 'sadf', '1sdfsa', 'adf', '2024-01-19 10:14:48', '1', '2024-01-19 10:14:48', '1', '%s', '4fsadf', '1235657898', 1, 'sadfr354', 'sdf456', '2024-01-19 18:13:29.000', '2024-01-19 18:13:31.000', '1', '2', '1', NULL, '2024-01-19 18:13:41', 2, '2024-01-19 18:14:47.000', 0, 1, 'sd', '1', '234365f', 'dfgfsh', '2', '230', '123', '123', '123', 123, '123', '2024-01-19 18:14:13', '2024-01-19 18:14:15', '2024-01-19 18:14:17', 2, 12, '1', '123wqr')",
+				accompanyId, strconv.Itoa(int(accompanyId))))
 	}
 	return strings.Join(values, ","), time.Since(start)
 }
--- shells/insert_bench.sh
+++ shells/insert_bench.sh
@@ -8,8 +8,6 @@ cd ../insert_benchmark
 # -timeout 1h: set the golang test timeout
 # insSize 1000: trying to insert 1000 rows
 
-# ********************************************* 1 session 1 terminal ********************************************
-
 # go test -run Test_Statement_CU_Insert -terminals 20 -sessions 20 -withTXN 1025 -insSize 102500 -timeout 1h
 
 ########################################## no pk
@@ -61,6 +59,3 @@ cd ../insert_benchmark
 # cluster pk - batch insert - on 4000W rows table
 # go test -run Test_Main -withPK 2 -terminals 40 -sessions 40 -withTXN 10000 -keepTbl 0 -insSize 40000000 -timeout 1h
 # go test -run Test_Main -withPK 2 -terminals 1 -sessions 1 -withTXN 100 -keepTbl 1 -insSize 1000000 -timeout 1h
-
-
-# ********************************************* multi session & terminal ********************************************
